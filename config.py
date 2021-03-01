5bb670f3e826d5e9a9207516d0657a4ff798e70b
chegrofelix <chegerofelix@gmail.com>
2021-03-01 23:35:48 +0300

    added config

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+
+class config:
+
+
+
+
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+    'development' : DevConfig,
+    'production'  : ProdConfig
+}
+
+
