3ebe298aed706f2ed1787985139c5bce93c00cbb
chegrofelix <chegerofelix@gmail.com>
2021-03-02 10:26:53 +0300

    created login template

--- config.py
+++ config.py
@@ -5,14 +5,22 @@ class config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-
-
 class ProdConfig(Config):
+      '''
+    Production configuration child class
 
+    Args:
+        Config: The parent configuration class with Gen config settings
+    '''
     pass
 
 class DevConfig(Config):
+      '''
+    Development configuration child class
 
+    Args:
+        Config: The parent configuration class with Gen config Settings
+    '''
     DEBUG = True
 
 config_options = {
