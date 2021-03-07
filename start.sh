20222749bd384013e20c1370fc8583e61035dea9
chegrofelix <chegerofelix@gmail.com>
2021-03-08 00:21:40 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME=felix.chegero@gmail.com
 export MAIL_PASSWORD=blackpanther19.
 export SECRET_KEY=infinitesimal
+export DATABASE_URL=postgresql+psycopg2://moringa:1234@localhost/chrome
 
 python3 manage.py server
