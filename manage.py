2ace12a9a4bb59da71499eea4bd87ff8d0104077
chegrofelix <chegerofelix@gmail.com>
2021-03-08 13:16:52 +0300

    production

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('produvtion')
+app = create_app('production')
 
 # Creating app instance
 manager = Manager(app)
