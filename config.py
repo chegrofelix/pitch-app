0c2d6a0aff090740681cd2f7df393c7f5812e8dd
chegrofelix <chegerofelix@gmail.com>
2021-03-08 13:11:32 +0300

    commit all

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     General configuration parent class
     '''
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-   
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY="infinitesimal"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos' 
     # email configuration
